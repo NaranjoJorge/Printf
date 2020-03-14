07725f4c3f303eed8bae898d7146b357bb60f9d3
NaranjoJorge <jpna188@gmail>
2020-03-14 22:43:17 +0000

    print_switch with mistakes.

--- printf_switch.c
+++ printf_switch.c
@@ -4,18 +4,28 @@ int _printf(const char *format, ...)
 {
 
 	int i = 0;
+	char *letra;
 	va_list list;
 
 	va_start(list, format);
 
-	if (*format[i] != '%')
-	{
-		for (i = 0; format && format[ );
-	}
-
-		if (*format[i] == %)
+	for (i = 0; format && *format[i]; i++)
 		{
-			switch (format[i + 1])
-			case "c": _putchar(va_arg(list, char*);
+
+			letra = va_arg(list, *format[i]);
+			if (*format[i] != '%')
+			{
+				_putchar (letra);
+			}
+
+			if (*format[i] == %)
+			{
+				switch (format[i + 1])
+				{
+				case "c":
+					_putchar (va_arg(list, *format[i + 1]));
+					break;
+				}
+			}
 		}
 }
