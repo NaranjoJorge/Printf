b347439fd75d21962ed60e6f2304f8dd1d7d52eb
santiagopemo <santiagopemo@gmail.com>
2020-03-15 02:18:28 +0000

    adding print functions

--- holberton.h
+++ holberton.h
@@ -2,19 +2,24 @@
 #define _HOLBERTON_H_
 #include <stddef.h>
 #include <stdio.h>
-
-
+#include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 
 /**
- * struct formato -
- * @z: char*
- * @(*fun):
+ * struct formato - structure for the function slector
+ * @z: format to print
+ * @fun: function that print the format
  */
 typedef struct formato{
 	char *z;
 	void (*fun)(va_list);
 } fmt;
+int _printf(const char *format, ...);
+int _putchar(char c);
+int fun_char(va_list c);
+int fun_string(va_list s);
+int fun_per(va_list c);
 
-#endif _HOLBERTON_H_
+#endif
