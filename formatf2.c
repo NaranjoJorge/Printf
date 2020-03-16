dc459e8a0585f28c58da202c5f5f13a55f7d832a
NaranjoJorge <jpna188@gmail>
2020-03-16 17:13:38 +0000

    Changes to formatf2.c

--- formatf2.c
+++ formatf2.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * fun_bin - function that prints binary
  * @b: va_list b
@@ -8,29 +7,26 @@
  */
 int fun_bin(va_list b)
 {
-	unsigned int num, max, i, sum;
-	unsigned int bits[32];
-	int count;
-
-	num = va_arg(b, unsigned int);
-	max = 2147483648;
-	bits[0] = num / max;
-	for (i = 1; i < 32; i++)
-	{
-		max /= 2;
-		bits[i] = (num / max) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += bits[i];
-		if (sum || i == 31)
-		{
-			count += _putchar(bits[i] + '0');
-		}
-	}
-	return (count);
+        unsigned int num, max, i, sum;
+        unsigned int bits[32];
+        int count;
+        num = va_arg(b, unsigned int);
+        max = 2147483648;
+        for (i = 0; i < 32; i++)
+        {
+                bits[i] = (num / max) % 2;
+                max /= 2;
+        }
+        for (i = 0, sum = 0, count = 0; i < 32; i++)
+        {
+                sum += bits[i];
+                if (sum || i == 31)
+                {
+                        count += _putchar(bits[i] + '0');
+                }
+        }
+        return (count);
 }
-
 /**
  * fun_uns - function that prints unsigned int
  * @u: va_list u
@@ -39,23 +35,20 @@ int fun_bin(va_list b)
  */
 int fun_uns(va_list u)
 {
-	unsigned int digits[10];
-	unsigned int j, teens, num, sum, count = 0;
-
-	num = va_arg(u, unsigned int);
-	teens = 1000000000;
-	digits[0] = num / teens;
-	for (j = 1; j < 10; j++)
-	{
-		teens /= 10;
-		digits[j] = (num / teens) % 10;
-
-	}
-	for (j = 0, sum = 0; j < 10; j++)
-	{
-		sum += digits[j];
-		if (digits[j] != 0 || j == 9)
-			count += _putchar(digits[j] + '0');
-	}
-	return (count);
+        unsigned int digits[10];
+        unsigned int j, teens, num, sum, count = 0;
+        num = va_arg(u, unsigned int);
+        teens = 1000000000;
+        for (j = 0; j < 10; j++)
+        {
+                digits[j] = (num / teens) % 10;
+                teens /= 10;
+        }
+        for (j = 0, sum = 0; j < 10; j++)
+        {
+                sum += digits[j];
+                if (digits[j] != 0 || j == 9)
+                        count += _putchar(digits[j] + '0');
+        }
+        return (count);
 }
